666ca355d5435b0fad767d70269bdcee71fa2aa4
r3j0 <readytojoin086@gmail.com>
2022-04-25 16:52:52 +0900

    edit p10799

--- p10799.c
+++ p10799.c
@@ -5,23 +5,34 @@
 
 int main(void) {
 	char str[100000];
-	int floor = 0;
-	int stick = 0;
+	int stick_floor[50000] = { 0, };
+	int stick_floor_now = -1;
+	int stick_floor_high = 0;
+	int stick_result = 0;
+
 	scanf("%s", str);
 	for (int i = 0; i < strlen(str); i++) {
 		if (str[i] == '(') {
-			floor++;
+			stick_floor_now++;
+			if (stick_floor_now > stick_floor_high)
+				stick_floor_high = stick_floor_now;
 		}
 		else { // ')'
-			floor--;
 			if (str[i - 1] == '(') {
-				stick += floor;
+				for (int i = 1; i <= stick_floor_now; i++) {
+					stick_floor[i]++;
+				}
+
 			}
-			if (str[i - 1] == ')') {
-				stick++;
+			else if (str[i - 1] == ')') {
+				stick_floor[stick_floor_now + 1]++;
 			}
+			stick_floor_now--;
 		}
 	}
-	printf("%d", stick);
+	for (int i = 1; i <= stick_floor_high; i++) {
+		stick_result += stick_floor[i];
+	}
+	printf("%d", stick_result);
 	return 0;
 }
