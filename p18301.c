291957bf68c611c5100cd84838a29f1cf6aff73c
r3j0 <readytojoin086@gmail.com>
2022-07-28 16:47:08 +0900

    add problem

---
+++ p18301.c
@@ -0,0 +1,9 @@
+// 18301 : Rats
+#include <stdio.h>
+
+int main(void) {
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+printf("%d",(((a+1)*(b+1))/(c+1)-1));
+return 0;
+}
---
+++ p3046.py
@@ -0,0 +1,3 @@
+# 3046 : R2
+a, b = map(int, input().split())
+print(b*2-a)
